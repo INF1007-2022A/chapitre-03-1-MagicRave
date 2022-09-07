29675a79ecfe8b27d4f77e7759c80b144798ea6b
Audry Darveau <audry.darveau@gmail.com>
2022-09-06 19:03:06 -0700

    complété

--- exercice.py
+++ exercice.py
@@ -3,34 +3,34 @@
 
 
 import math
+from xml.etree.ElementTree import PI
 
 def square_root(a: float) -> float:
-    return 0.0
+    return math.sqrt(a)
 
 
 def square(a: float) -> float:
-    return 0.0
+    return math.pow(a, 2)
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return ((a+b+c)/3)
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    return (((angle_degs + (angle_mins/60) + (angle_secs/3600))*math.pi)/180)
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    return (angle_rads*180/math.pi), ((angle_rads*180/math.pi)/60), ((angle_rads*180/math.pi)/3600)
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature*(9/5))+32
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
-
+    return (temperature-32)/9*5
 
 def main() -> None:
     print(f"La racine carré de 144 est : {square_root(144)}")
